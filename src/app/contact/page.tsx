f1af57d273f87cc568cdc332e0c8e677ba0db23b
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-18 11:11:49 +0700

    move init

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1 className="text-3xl font-semibold">Contact</h1>
+            <p className="text-lg">Email: <a href="mailto:kurtzjeremy126@gmail.com">kurtzjeremy126@gmail.com</a></p>
+            <p className="text-lg">Linkedin: <a href="https://www.linkedin.com/in/jeremymkurtz/">jeremymkurtz</a></p>
+            <p className="text-lg">Github: <a href="https://www.github.com/jeremymkurtz">jeremymkurtz</a></p>
+            <p className="text-lg">Phone Number: <a href="tel:3127184905">+1[312]-718-4905</a></p>
+        </div>
+    )
+}
