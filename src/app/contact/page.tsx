0411712c93e8945af1bfce2f150aedc4e20e8e36
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-19 22:44:21 +0700

    Changed contact links to Next/Link Component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export default function Contact() {
     return (
         <div>
             <h1 className="text-3xl font-semibold">Contact</h1>
-            <p className="text-lg">Email: <a href="mailto:kurtzjeremy126@gmail.com">kurtzjeremy126@gmail.com</a></p>
-            <p className="text-lg">Linkedin: <a href="https://www.linkedin.com/in/jeremymkurtz/">jeremymkurtz</a></p>
-            <p className="text-lg">Github: <a href="https://www.github.com/jeremymkurtz">jeremymkurtz</a></p>
-            <p className="text-lg">Phone Number: <a href="tel:3127184905">+1[312]-718-4905</a></p>
+            <p className="text-lg">Email: <Link href="mailto:kurtzjeremy126@gmail.com"><a>kurtzjeremy126@gmail.com</a></Link></p>
+            <p className="text-lg">Linkedin: <Link href="https://www.linkedin.com/in/jeremymkurtz/"><a>jeremymkurtz</a></Link></p>
+            <p className="text-lg">Github: <Link href="https://www.github.com/jeremymkurtz"><a>jeremymkurtz</a></Link></p>
+            <p className="text-lg">Phone Number: <Link href="tel:3127184905"><a>+1[312]-718-4905</a></Link></p>
         </div>
     )
 }
