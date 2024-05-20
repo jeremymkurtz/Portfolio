ee3031a63db520f1a774abe74f6696fd95660be3
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-20 17:44:15 +0700

    You can now click stuff on the actual page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@
 
 import {motion} from "framer-motion";
 import Link from "next/link";
+import React from "react";
 
 const NavLink = [
     {
@@ -53,14 +54,29 @@ function MenuContent(props:{isOpen: boolean, toggle: () => void}) {
     )
 }
 
+
 export default function Menu(props: { toggle: () => void, isOpen: boolean }) {
+    const [display, setDisplay] = React.useState('hidden');
+
+    const handleAnimationComplete = () => {
+        if (!props.isOpen) {
+            setDisplay('hidden');
+        }
+    };
+
+    React.useEffect(() => {
+        if (props.isOpen) {
+            setDisplay('block');
+        }
+    }, [props.isOpen]);
+
     return (
         <motion.div
             id="menu"
-            className={`z-20 top-0 left-0 fixed h-screen w-full pointer-events-auto block opacity-0`}
+            className={`z-20 top-0 left-0 fixed h-screen w-full pointer-events-auto ${display} opacity-0`}
             style={{WebkitBackdropFilter: "blur(5px)", backdropFilter: "blur(5px)"}}
             animate={{ opacity: props.isOpen ? 1 : 0 ,background: props.isOpen ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0)"}}
-            transition={{ duration: 0.2}} // Blur happens first when opening, last when closing
+            transition={{ duration: 0.2, onComplete: handleAnimationComplete }} // Blur happens first when opening, last when closing
         >
             <div id="menu-content"
                  className="relative flex flex-col items-center justify-center h-full overflow-hidden">
