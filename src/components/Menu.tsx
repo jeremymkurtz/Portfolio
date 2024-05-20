396e0eaf49bbfec3379fc01d603ca464c8035cdc
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-20 15:56:48 +0700

    adjusted the implementation using variants instead

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import Link from "next/link";
-import {useEffect, useState} from "react";
 
 const NavLink = [
     {
@@ -24,29 +23,24 @@ const NavLink = [
 ]
 
 
+
+
+const variants = {
+  open: { y: 0, opacity: 1 },
+  closed: { y: -30, opacity: 0 },
+};
+
 function MenuContent(props:{isOpen: boolean, toggle: () => void}) {
-    const [animationState, setAnimationState] = useState({ y: -50, opacity: 0 });
-
-    useEffect(() => {
-        setTimeout(() => { // Delay the content animation
-            if (props.isOpen) {
-                setAnimationState({ y: 0, opacity: 1 });
-            } else {
-                setAnimationState({ y: -50, opacity: 1 });
-            }
-        }, props.isOpen ? 300 : 0); // Delay is 300ms when opening, no delay when closing
-    }, [props.isOpen]);
-
     return (
         <>
             {NavLink.map((link, index) => {
                 return (
                     <div className={"overflow-hidden"} key={index}>
                         <motion.div
-                            initial={{y: -50, opacity: 1}}
-                            animate={animationState}
-                            exit={{y: -50, opacity: 1}}
-                            transition={{type: 'spring', stiffness: 100, damping: 20, delay: index * 0.1, duration: 0.1}}
+                            initial="closed"
+                            animate={props.isOpen ? "open" : "closed"}
+                            variants={variants}
+                            transition={{type: 'spring', stiffness: 100, damping: 20, delay: index * 0.1, duration: 0.4}}
                         >
                             <Link href={link.href} onClick={props.toggle}>
                                 {link.text}
@@ -65,7 +59,7 @@ export default function Menu(props: { toggle: () => void, isOpen: boolean }) {
             id="menu"
             className={`z-10 top-0 left-0 fixed h-screen w-full pointer-events-auto block backdrop-blur-sm opacity-0`}
             animate={{ opacity: props.isOpen ? 1 : 0 ,background: props.isOpen ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0)"}}
-            transition={{ duration: 0.3}} // Blur happens first when opening, last when closing
+            transition={{ duration: 0.2}} // Blur happens first when opening, last when closing
         >
             <div id="menu-content"
                  className="relative flex flex-col items-center justify-center h-full overflow-hidden">
