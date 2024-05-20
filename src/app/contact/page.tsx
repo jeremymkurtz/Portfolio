49306ad6cd0d45fec3117c81aa28a23da003d36a
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-20 17:51:30 +0700

    Made contact page slightly more interacitve

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,10 @@ export default function Contact() {
     return (
         <div>
             <h1 className="text-3xl font-semibold">Contact</h1>
-            <p className="text-lg">Email: <Link href="mailto:kurtzjeremy126@gmail.com">kurtzjeremy126@gmail.com</Link></p>
-            <p className="text-lg">Linkedin: <Link href="https://www.linkedin.com/in/jeremymkurtz/">jeremymkurtz</Link></p>
-            <p className="text-lg">Github: <Link href="https://www.github.com/jeremymkurtz">jeremymkurtz</Link></p>
-            <p className="text-lg">Phone Number: <Link href="tel:3127184905">+1[312]-718-4905</Link></p>
+            <p className="text-lg underline">Email: <Link href="mailto:kurtzjeremy126@gmail.com">kurtzjeremy126@gmail.com</Link></p>
+            <p className="text-lg underline">Linkedin: <Link href="https://www.linkedin.com/in/jeremymkurtz/">jeremymkurtz</Link></p>
+            <p className="text-lg underline">Github: <Link href="https://www.github.com/jeremymkurtz">jeremymkurtz</Link></p>
+            <p className="text-lg underline">Phone Number: <Link href="tel:+1(312)718-4905">+1[312]-718-4905</Link></p>
         </div>
     )
 }
