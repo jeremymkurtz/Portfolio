8f484f032a9b6788d46043816d165d9e92178b74
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-05-21 18:27:01 +0700

    Added Logo

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Menu from "@/components/ui/navigation/Menu";
 import Nav from "@/components/ui/navigation/header/Nav";
 import ThemeToggle from "@/components/ui/navigation/header/ThemeToggle";
+import Logo from "@/components/ui/navigation/header/logo";
 
 
 const UserInterface = () => {
@@ -12,13 +13,14 @@ const UserInterface = () => {
     };
     return (
         <>
-            <header className={"fixed z-50 top-12 left-0 right-0"}>
-                <div id={"header content"} className={"flex flex-row px-20 justify-between"}>
+            <header className={"fixed z-50 top-6 sm:top-24 left-0 right-0"}>
+                <div id={"header content"} className={"flex flex-row px-7 sm:px-24 justify-between"}>
                     <div id={"left"} className={"flex flex-row"}>
-                        <Nav size={40} toggle={toggle} isOpen={isOpen}/>
+                        <Logo/>
                     </div>
-                    <div id={"right"} className={"flex flex-row items-center justify-content"}>
-                        <ThemeToggle size={30}/>
+                    <div id={"right"} className={"flex flex-row items-center justify-content space-x-6"}>
+                        <ThemeToggle size={20}/>
+                        <Nav size={40} toggle={toggle} isOpen={isOpen}/>
                     </div>
                 </div>
             </header>
