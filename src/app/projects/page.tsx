27ba6a53406cd4331ab2aba5d166351330116faf
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-02 00:56:27 +0700

    add page wrapper on projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,14 @@
 import WidenText from "@/components/text/WidenAnimator";
+import PageWrapper from "@/components/PageWrapper";
 
 export default function Work(){
     return (
-        <div className={"gap-2"}>
+        <PageWrapper center={true} >
+<div className={"gap-2"}>
             <WidenText indexes={[0,2]} text={"Work"} cn={"text-4xl font-black uppercase"}/>
             <WidenText indexes={[2,3]} text={"Projects"} cn={"text-4xl font-black uppercase"}/>
         </div>
+        </PageWrapper>
+        
     )
 }
