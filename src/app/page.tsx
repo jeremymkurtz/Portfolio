c9bcf9a223163ba8e763a1d1f9ef3ac99bbe2aa9
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-01 22:27:01 +0700

    Home page semi-updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 "use client"
-import Image from "next/image";
-import WidenAnimator from "@/components/WidenAnimator";
+import { FlipWords } from "@/components/text/FlipWords";
+import PageWrapper from "@/components/PageWrapper";
+import ShinyButton from "@/components/ui/button/ShinyButton";
+import LinkUnderline from "@/components/text/LinkUnderline";
+import { SlArrowRightCircle } from "react-icons/sl";
+
+const RightArrow = <SlArrowRightCircle className="inline" />
+
 
 export default function Home() {
+  const roles = ["Full Stack Dev", "designer", "student", "badminton player", "gamer", "manga reader"]
   return (
-   <div>
-       <WidenAnimator indexes={[1]} text={"Home"} cn={"text-4xl font-black uppercase"}/>
+    <PageWrapper >
+   <div className="text-left font-black font-Anybody uppercase fixed">
+       <h1 className="text-4xl font-black font-Anybody uppercase"> Hey my name is Jeremy,</h1>
+       <h1 className="text-4xl"> I am a<FlipWords words={roles} /></h1>
+       <div className="flex flex-row gap-4">
+          <LinkUnderline href="/projects" cn="font-jakarta">{RightArrow} See my work</LinkUnderline>
+          <LinkUnderline href="/about" cn="font-jakarta">{RightArrow} More about me</LinkUnderline>
+       </div>
+       
+      <ShinyButton cn="w-fit font-jakarta">Click Me</ShinyButton>
    </div>
+   
+   </PageWrapper>
   );
 }
