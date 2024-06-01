163ba21168e6f93a3dd9f0f8652148f79eb8107b
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-02 01:42:04 +0700

    Comment out example

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,7 +21,7 @@ export default function Contact() {
                 </LinkUnderline>
             </div>
             
-            <Example />
+            {/* <Example /> */}
         </div>
     )
 }
