44eac94cbf6c1a2ec1b8a735914ed221a39c2cde
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-01 22:24:37 +0700

    added shiny button

---
+++ src/components/ui/button/ShinyButton.tsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import React, {ReactNode} from "react";
+
+function ShinyButton(props:{cn?:string, children: ReactNode }) {
+    return (
+        <motion.button
+            initial={{ "--x": "100%", scale: 1 }} // Its not broken
+            animate={{ "--x": "-100%" }} // Its not broken
+            whileTap={{ scale: 0.97 }}
+            transition={{
+                repeat: Infinity,
+                repeatType: "loop",
+                repeatDelay: 1,
+                type: "spring",
+                stiffness: 20,
+                damping: 15,
+                mass: 2,
+                scale: {
+                    type: "spring",
+                    stiffness: 10,
+                    damping: 5,
+                    mass: 0.1,
+                },
+            }}
+            className={`px-6 py-2 rounded-md relative dark:radial-gradient ${props.cn ? props.cn : ""}`} //radial-gradient
+        >
+      <span className={`text-primary dark:text-primaryDark tracking-wide font-Anybody block relative linear-mask `}>
+        {props.children}
+      </span>
+            <span className="block absolute inset-0 rounded-md p-[2px] linear-overlay" />
+        </motion.button>
+    );
+};
+
+export default ShinyButton;
