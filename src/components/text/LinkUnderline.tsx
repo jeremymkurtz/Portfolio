226fb6b3e49300794bf28cafc6fac44dc080856a
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-01 22:26:37 +0700

    Refactor LinkUnderline

---
+++ src/components/text/LinkUnderline.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function LinkUnderline(props:{cn?:string, id?:string,children:React.ReactNode, href:string, useLink?:boolean, onClick?:()=>void}){
+    return <div id={props.id ? props.id : undefined} className={`relative group w-fit ${props.cn ? props.cn : ""}`}>
+        {props.useLink ? <Link href={props.href} onClick={props.onClick}>
+            {props.children}
+        </Link> :
+            <a href={props.href}>{props.children} </a>}
+        <span
+            className="absolute left-0 bottom-0 w-0 h-0.5 bg-primary dark:bg-primaryDark transition-all group-hover:w-full"></span>
+    </div>;
+}
