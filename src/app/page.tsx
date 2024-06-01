407666169b0d5f21c1f393700c60917477a21e46
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-02 01:42:17 +0700

    Reorganize layout of home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,18 @@ export default function Home() {
   const roles = ["Full Stack Dev", "designer", "student", "badminton player", "gamer", "manga reader"]
   return (
     <PageWrapper >
-   <div className="text-left font-black font-Anybody uppercase fixed">
-       <h1 className="text-4xl font-black font-Anybody uppercase"> Hey my name is Jeremy,</h1>
-       <h1 className="text-4xl"> I am a<FlipWords words={roles} /></h1>
-       <div className="flex flex-row gap-4">
+   <div className="text-left font-black font-Anybody uppercase flex flex-col gap-4">
+      <div>
+        <h1 className="text-4xl font-black font-Anybody uppercase"> Hey my name is Jeremy,</h1>
+        <h1 className="text-4xl"> I am a<FlipWords words={roles} /></h1>
+        <div className="flex flex-row gap-4">
           <LinkUnderline href="/projects" cn="font-jakarta">{RightArrow} See my work</LinkUnderline>
           <LinkUnderline href="/about" cn="font-jakarta">{RightArrow} More about me</LinkUnderline>
        </div>
+      </div>
        
-      <ShinyButton cn="w-fit font-jakarta">Click Me</ShinyButton>
+       
+      <ShinyButton cn="w-fit font-jakarta">I don't do anything yet</ShinyButton>
    </div>
    
    </PageWrapper>
