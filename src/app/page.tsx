6520d52e8aa41ba294d8531dc5a6995db547f98d
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-03 19:50:26 +0700

    used sparkle component on comma in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import ShinyButton from "@/components/ui/button/ShinyButton";
 import LinkUnderline from "@/components/text/LinkUnderline";
 import { SlArrowRightCircle } from "react-icons/sl";
 import { useAuroraContext } from "@/contexts/AuroraContext";
+import AuroraWord from "@/components/text/AuroraWord";
+import Sparkle from "@/components/home/sparkle";
 
 const RightArrow = <SlArrowRightCircle className="inline" />
 
@@ -18,9 +20,10 @@ export default function Home() {
       
    <div className="text-left font-black font-Anybody uppercase flex flex-col gap-4 justify-center sm:items-center">
       <div className="justify-center items-center text-3xl sm:text-4xl md:text-5xl">
-        <h1 className="text-balance"> Hey my name is Jeremy,</h1>
+        <h1 className="text-balance flex hover:cursor-pointer" onClick={toggleAurora}> Hey my name is Jeremy <Sparkle>,</Sparkle></h1>
         <div className="mx-auto">
-          I am a
+            I am a
+
           <FlipWords words={roles} className="pl-2 md:pl-3 w-fit"/>
         </div>
        
