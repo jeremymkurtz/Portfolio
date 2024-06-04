102b8ac1cd4ca078c996e5ce572943cae2922323
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-04 16:45:55 +0700

    Meteors Change According to theme

--- src/components/ui/meteors.tsx
+++ src/components/ui/meteors.tsx
@@ -19,8 +19,8 @@ export const Meteors = ({
                 <span
                     key={"meteor" + idx}
                     className={cn(
-                        "animate-meteor-effect absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-white shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
-                        "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#FFFFFF] before:to-transparent",
+                        "animate-meteor-effect absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-white shadow-[0_0_0_1px_#60688710] dark:shadow-[0_0_0_1px_#F3F2F910] rotate-[215deg]",
+                        "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-primary dark:before:from-primaryDark before:to-transparent",
                         className
                     )}
                     style={{
