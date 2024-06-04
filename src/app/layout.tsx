1ee5938d92430a6bdb0ebd7a28f8b6414ee2b107
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-06-05 00:23:16 +0700

    first attempt getting new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,28 @@ import {anybody, inter, jakarta} from "./fonts";
 import type { Viewport } from 'next'
 import { Providers } from './providers'
 import PageLoading from "@/components/PageLoading";
+import JK_light from "../../public/images/JK-Circle Modern-extrasmall.png";
 
+import JK_dark from "../../public/images/JK-Circle Modern-small-dark.png";
 
 
 export const metadata: Metadata = {
     title: "Jeremy Kurtz",
     description: "Jeremy Kurtz's personal website",
+    icons: {
+        icon: [
+            {
+                media: '(prefers-color-scheme: light)',
+                url: '../../public/images/JK-Circle Modern-extrasmall.png',
+                href: '../../public/images/JK-Circle Modern-extrasmall.png',
+            },
+            {
+                media: '(prefers-color-scheme: dark)',
+                url: '../../public/images/JK-Circle Modern-extrasmall.png',
+                href: '../../public/images/JK-Circle Modern-extrasmall.png',
+            },
+        ],
+    },
 };
 
 
@@ -25,17 +41,17 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
         <body className={`${inter.variable} ${anybody.variable} ${jakarta.variable} h-svh bg-background dark:bg-backgroundDark`}
               style={{WebkitOverflowScrolling: 'auto'}}
-              >
-               <PageLoading/> 
+        >
+        <PageLoading/>
         <Providers>
             <UserInterface/>
-             <main className={" text-primary dark:text-primaryDark w-full h-full flex justify-center items-center "}>
+            <main className={" text-primary dark:text-primaryDark w-full h-full flex justify-center items-center "}>
 
-                    {children}
+                {children}
 
             </main>
         </Providers>
- 
+
         </body>
         </html>
     );
